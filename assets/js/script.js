ad16aa9b1b7f951512eac628145ff0e395b9e1c0
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-05-24 16:27:43 -0400

    mvp

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,34 @@ cleanScreen = () => {
     weatherCardsContainer.innerHTML = '';
 }
 
+getCurrentDayWeather = (response) => {    
+    
+    //create elements of the current day weather
+    var showCityName = document.createElement('h2');
+    showCityName.textContent = response.name + ' ' + new Date().toLocaleDateString();
+    currentDayWeather.appendChild(showCityName);
+
+    var weatherIcon = document.createElement('img');
+    var iconCode = response.weather[0].icon;
+    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+    weatherIcon.setAttribute('src', iconUrl);
+    currentDayWeather.appendChild(weatherIcon);
+
+    var currentTemp = document.createElement('p');
+    currentTemp.textContent = 'Temp: ' + response.main.temp + ' ' + '\xB0' + 'F';
+    currentDayWeather.appendChild(currentTemp);
+
+    var currentWind = document.createElement('p');
+    currentWind.textContent = 'Wind: ' + response.wind.speed + ' MPH';
+    currentDayWeather.appendChild(currentWind);
+
+    var currentHumidity = document.createElement('p');
+    currentHumidity.textContent = 'Humidity: ' + response.main.humidity + '%';
+    currentDayWeather.appendChild(currentHumidity);
+}
+
 createCityWeather = (e) => {
+    
     //check and see if the item clicked was a button
     if(e.target.nodeName.toLowerCase() !== 'button') {
         return;
@@ -40,109 +67,125 @@ createCityWeather = (e) => {
         if(e.target.id == 'search-btn') {
             //create button of the searched city so that they can go back to it
             var createCityBtn = document.createElement('button');
-            createCityBtn.classList = 'btn btn-secondary col-12';
+            createCityBtn.classList = 'btn btn-secondary col-lg-12 col-md-6 col-sm-3';
             createCityBtn.textContent = response.name;
             existingCities.appendChild(createCityBtn);
+
+            saveCityToLocalStorage(response);
         }
 
-        //create elements of the current day weather
-        var showCityName = document.createElement('h2');
-        showCityName.textContent = response.name + ' ' + new Date().toLocaleDateString();
-        currentDayWeather.appendChild(showCityName);
-
-        var currentTemp = document.createElement('p');
-        currentTemp.textContent = 'Temp: ' + response.main.temp + ' F';
-        currentDayWeather.appendChild(currentTemp);
-
-        var currentWind = document.createElement('p');
-        currentWind.textContent = 'Wind: ' + response.wind.speed + ' MPH';
-        currentDayWeather.appendChild(currentWind);
-
-        var currentHumidity = document.createElement('p');
-        currentHumidity.textContent = 'Humidity: ' + response.main.humidity + '%';
-        currentDayWeather.appendChild(currentHumidity);
+        getCurrentDayWeather(response);
 
         uvIndex = document.createElement('p');
         uvIndex.textContent = 'UV Index: ';
         
         var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=' + response.coord.lat + '&lon=' + response.coord.lon + '&exclude=current,minutely,hourly,alerts&appid=f09c956aca2ff808cc4ce08e3330fb56';
 
-        return fetch(apiUrl)
+        return fetch(apiUrl);
     })
     .then((response) => {
         return response.json();
     })
     .then((response) => {
 
-        //create current day uv index
-        var uvSpan = document.createElement('span');
-        uvSpan.textContent = response.daily[0].uvi
-        
-        if(response.daily[0].uvi <= 2 ) {
-            uvSpan.classList = 'uv-index favorable-uv';
-        }
-        else if(response.daily[0].uvi >= 7) {
-            uvSpan.classList = 'uv-index severe-uv';
-        }
-        else {
-            uvSpan.classList = 'uv-index moderate-uv';
-        }
-        uvIndex.appendChild(uvSpan);
-        currentDayWeather.appendChild(uvIndex);
+        createUvSpan(response, uvIndex);
 
         return response;
-        
     })
     .then((response) => {
-        //create h3 explaining the days forcasted
-        var daysForecast = document.createElement('h3');
-        daysForecast.textContent = '5 Day Forecast:';
-        weatherCardsContainer.appendChild(daysForecast);
-        
-        //for loop to create weather cards
-        for(i = 1; i < response.daily.length - 2; i++) {
-            
-            var weatherCard = document.createElement('div');
-            weatherCard.classList = 'card bg-dark text-white col-2';
-
-            var cardBody = document.createElement('div');
-            cardBody.classList = 'card-body';
-            
-            var cardTitle = document.createElement('h4');
-            //figure out how to increment day by 1
-            cardTitle.textContent = '5/24/21';
-            cardBody.appendChild(cardTitle);
-
-            var cardTemp = document.createElement('p');
-            cardTemp.textContent = 'Temp: ' + response.daily[i].temp.day + ' F';
-            cardTemp.classList = 'card-text';
-            cardBody.appendChild(cardTemp);
-
-            var cardHumidity = document.createElement('p');
-            cardHumidity.textContent = 'Humidity: ' + response.daily[i].humidity + '%';
-            cardHumidity.classList = 'card-text';
-            cardBody.appendChild(cardHumidity);
-
-            var cardWind = document.createElement('p');
-            cardWind.textContent = 'Wind: ' + response.daily[i].wind_speed + ' MPH';
-            cardWind.classList = 'card-text';
-            cardBody.appendChild(cardWind);
-
-            weatherCard.appendChild(cardBody);
-            weatherCardsContainer.appendChild(weatherCard);
-        }
+        createWeatherCards(response);
     })
 
 }
 
+createUvSpan = (response, uvIndex) => {
+    //create current day uv index
+    var uvSpan = document.createElement('span');
+    uvSpan.textContent = response.daily[0].uvi
+    
+    if(response.daily[0].uvi <= 2 ) {
+        uvSpan.classList = 'uv-index favorable-uv';
+    }
+    else if(response.daily[0].uvi >= 7) {
+        uvSpan.classList = 'uv-index severe-uv';
+    }
+    else {
+        uvSpan.classList = 'uv-index moderate-uv';
+    }
+    uvIndex.appendChild(uvSpan);
+    currentDayWeather.appendChild(uvIndex);
+}
 
+createWeatherCards = (response) => {
 
+    //create h3 explaining the days forcasted
+    var daysForecast = document.createElement('h3');
+    daysForecast.textContent = '5 Day Forecast:';
+    weatherCardsContainer.appendChild(daysForecast);
+    
+    //for loop to create weather cards
+    for(i = 1; i < response.daily.length - 2; i++) {
+        
+        var weatherCard = document.createElement('div');
+        weatherCard.classList = 'card bg-dark text-white col-lg-2 col-md-3 col-sm-6 m-lg-3 m-md-2 m-sm-1';
 
+        var cardBody = document.createElement('div');
+        cardBody.classList = 'card-body';
+        
+        var cardTitle = document.createElement('h4');
+        //figure out how to increment day by 1
+        var currentDate = new Date();
+        var forecastDays = new Date(currentDate.getTime() + (i * 86400000));
+        cardTitle.textContent = forecastDays.toLocaleDateString();
+        cardBody.appendChild(cardTitle);
 
+        var cardIcon = document.createElement('img');
+        var iconCode = response.daily[i].weather[0].icon;
+        var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+        cardIcon.setAttribute('src', iconUrl);
+        cardBody.appendChild(cardIcon);
 
+        var cardTemp = document.createElement('p');
+        cardTemp.textContent = 'Temp: ' + response.daily[i].temp.day + ' ' + '\xB0' + 'F';
+        cardTemp.classList = 'card-text';
+        cardBody.appendChild(cardTemp);
 
+        var cardHumidity = document.createElement('p');
+        cardHumidity.textContent = 'Humidity: ' + response.daily[i].humidity + '%';
+        cardHumidity.classList = 'card-text';
+        cardBody.appendChild(cardHumidity);
 
+        var cardWind = document.createElement('p');
+        cardWind.textContent = 'Wind: ' + response.daily[i].wind_speed + ' MPH';
+        cardWind.classList = 'card-text';
+        cardBody.appendChild(cardWind);
 
+        weatherCard.appendChild(cardBody);
+        weatherCardsContainer.appendChild(weatherCard);
+    }
+}
+
+saveCityToLocalStorage = (response) => {
+    var cityStorage = JSON.parse(localStorage.getItem('cities')) || [];
+
+    cityStorage.push(response.name);
+    
+    localStorage.setItem('cities', JSON.stringify(cityStorage));
+}
+
+loadSearchedCitiesOnPageLoad = () => {
+    var cityStorage = JSON.parse(localStorage.getItem('cities')) || [];
+
+    for(i = 0; i < cityStorage.length; i++) {
+        //create button of the searched city so that they can go back to it
+        var createCityBtn = document.createElement('button');
+        createCityBtn.classList = 'btn btn-secondary col-12';
+        createCityBtn.textContent = cityStorage[i];
+        existingCities.appendChild(createCityBtn);
+    }
+}
+
+loadSearchedCitiesOnPageLoad();
 
 
 //add event listener to all buttons
