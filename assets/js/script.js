8b923ee79dc1c4acf5a1a59d1f9087ae00655b13
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-05-24 19:23:06 -0400

    working website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,79 @@
 var searchBtn = document.querySelector('#search-btn');
 var existingCities = document.querySelector('#searched-cities')
-var currentDayWeather = document.querySelector('#current-day');
-var weatherCardsContainer = document.querySelector('#card-container');
-var allBtns = document.querySelectorAll('.btn');
+var weatherInfoContainer = document.querySelector('#weather-info-container');
+var clearHistBtn = document.querySelector('#clear-history');
 
 
-cleanScreen = () => {
-    currentDayWeather.innerHTML = '';
+deleteHistory = () => {
+    localStorage.removeItem('cities');
 
-    weatherCardsContainer.innerHTML = '';
+    existingCities.innerHTML = '';
 }
 
-getCurrentDayWeather = (response) => {    
+cleanScreen = () => {
+    weatherInfoContainer.innerHTML = '';
+}
+
+getCurrentDayWeather = (response, e) => {    
     
+    if(e.target.id == 'search-btn') {
+        
+        var cityName = document.querySelector('#search-bar').value.trim().toLowerCase();
+        var cityWords = cityName.split(' ');
+        for (i = 0; i < cityWords.length; i++) {
+            cityWords[i] = cityWords[i][0].toUpperCase() + cityWords[i].substr(1);
+        }
+
+        var userInput = cityWords.join(' ');
+        
+        //create button of the searched city so that they can go back to it
+        var createCityBtn = document.createElement('button');
+        createCityBtn.classList = 'btn btn-secondary col-lg-12 col-md-6 col-sm-3';
+        createCityBtn.textContent = userInput;
+        existingCities.appendChild(createCityBtn);
+
+        
+
+        saveCityToLocalStorage(userInput);
+    }
+
+    else {
+        var userInput = e.target.innerText;
+    }
+
+    currentDayWeather = document.createElement('div');
+    currentDayWeather.classList = 'col-12 container';
+    currentDayWeather.id = 'current-day';
+    weatherInfoContainer.appendChild(currentDayWeather);
+
+
     //create elements of the current day weather
     var showCityName = document.createElement('h2');
-    showCityName.textContent = response.name + ' ' + new Date().toLocaleDateString();
+    showCityName.textContent = userInput + ' ' + new Date().toLocaleDateString();
     currentDayWeather.appendChild(showCityName);
 
     var weatherIcon = document.createElement('img');
-    var iconCode = response.weather[0].icon;
+    var iconCode = response.daily[0].weather[0].icon;
     var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
     weatherIcon.setAttribute('src', iconUrl);
     currentDayWeather.appendChild(weatherIcon);
 
     var currentTemp = document.createElement('p');
-    currentTemp.textContent = 'Temp: ' + response.main.temp + ' ' + '\xB0' + 'F';
+    currentTemp.textContent = 'Temp: ' + response.daily[0].temp.day + ' ' + '\xB0' + 'F';
     currentDayWeather.appendChild(currentTemp);
 
     var currentWind = document.createElement('p');
-    currentWind.textContent = 'Wind: ' + response.wind.speed + ' MPH';
+    currentWind.textContent = 'Wind: ' + response.daily[0].wind_speed + ' MPH';
     currentDayWeather.appendChild(currentWind);
 
     var currentHumidity = document.createElement('p');
-    currentHumidity.textContent = 'Humidity: ' + response.main.humidity + '%';
+    currentHumidity.textContent = 'Humidity: ' + response.daily[0].humidity + '%';
     currentDayWeather.appendChild(currentHumidity);
-}
 
-createCityWeather = (e) => {
-    
-    //check and see if the item clicked was a button
-    if(e.target.nodeName.toLowerCase() !== 'button') {
-        return;
-    }
-
-    cleanScreen();
-
-    //change the city name variable based on what the user clicked
-    if(e.target.id == 'search-btn') {
-        var cityName = document.querySelector('#search-bar').value;
-    }
-    else {
-        var cityName = e.target.innerText
-    } 
-    
-    
-    var urlForData = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=f09c956aca2ff808cc4ce08e3330fb56'
-    
-
-    fetch(urlForData)
-    .then((response) =>  {
-        return response.json();
-    })
-    .then((response) => {
-
-        if(e.target.id == 'search-btn') {
-            //create button of the searched city so that they can go back to it
-            var createCityBtn = document.createElement('button');
-            createCityBtn.classList = 'btn btn-secondary col-lg-12 col-md-6 col-sm-3';
-            createCityBtn.textContent = response.name;
-            existingCities.appendChild(createCityBtn);
-
-            saveCityToLocalStorage(response);
-        }
-
-        getCurrentDayWeather(response);
-
-        uvIndex = document.createElement('p');
-        uvIndex.textContent = 'UV Index: ';
-        
-        var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=' + response.coord.lat + '&lon=' + response.coord.lon + '&exclude=current,minutely,hourly,alerts&appid=f09c956aca2ff808cc4ce08e3330fb56';
-
-        return fetch(apiUrl);
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-
-        createUvSpan(response, uvIndex);
-
-        return response;
-    })
-    .then((response) => {
-        createWeatherCards(response);
-    })
-
-}
-
-createUvSpan = (response, uvIndex) => {
     //create current day uv index
+    var uvIndex = document.createElement('p');
+    uvIndex.textContent = 'UV Index: ';
+    
     var uvSpan = document.createElement('span');
     uvSpan.textContent = response.daily[0].uvi
     
@@ -116,7 +90,71 @@ createUvSpan = (response, uvIndex) => {
     currentDayWeather.appendChild(uvIndex);
 }
 
+createCityWeather = (e) => {
+    console.log(e);
+    //check and see if the item clicked was a button
+    if(e.target.nodeName.toLowerCase() !== 'button') {
+        console.log('not button');
+        return;
+    }
+
+    cleanScreen();
+
+    //change the city name variable based on what the user clicked
+    if(e.target.id == 'search-btn') {
+        var cityName = document.querySelector('#search-bar').value;
+    }
+    else {
+        var cityName = e.target.innerText
+    } 
+    
+    var urlForData = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=f09c956aca2ff808cc4ce08e3330fb56'
+    
+
+    fetch(urlForData)
+    .then((response) =>  {
+        //check to see if response is ok
+        if(response.ok) {
+            return response.json();
+        }
+        else {
+            alert('Error: Unable to find city. Please try to search again:)')
+        }
+        
+    })
+    .then((response) => {
+
+
+        
+        var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=' + response.coord.lat + '&lon=' + response.coord.lon + '&exclude=current,minutely,hourly,alerts&appid=f09c956aca2ff808cc4ce08e3330fb56';
+
+        return fetch(apiUrl);
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+
+        getCurrentDayWeather(response, e);
+
+        return response;
+    })
+    .then((response) => {
+        createWeatherCards(response);
+    })
+    .catch((error) => {
+        alert("Our servers are down please try again another time :)");
+    });
+
+}
+
 createWeatherCards = (response) => {
+
+    var weatherCardsContainer = document.createElement('div');
+    weatherCardsContainer.classList = 'row col-12';
+    weatherCardsContainer.id = 'card-container';
+    weatherInfoContainer.appendChild(weatherCardsContainer);
+
 
     //create h3 explaining the days forcasted
     var daysForecast = document.createElement('h3');
@@ -165,10 +203,10 @@ createWeatherCards = (response) => {
     }
 }
 
-saveCityToLocalStorage = (response) => {
+saveCityToLocalStorage = (userInput) => {
     var cityStorage = JSON.parse(localStorage.getItem('cities')) || [];
 
-    cityStorage.push(response.name);
+    cityStorage.push(userInput);
     
     localStorage.setItem('cities', JSON.stringify(cityStorage));
 }
@@ -187,7 +225,7 @@ loadSearchedCitiesOnPageLoad = () => {
 
 loadSearchedCitiesOnPageLoad();
 
-
 //add event listener to all buttons
 searchBtn.addEventListener('click', createCityWeather);
 existingCities.addEventListener('click', createCityWeather);
+clearHistBtn.addEventListener('click', deleteHistory);
